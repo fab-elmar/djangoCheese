5de28dda33c7e72da42126dc3e8c6b7a43d6ee9f
fabian-elmar <fabsiban09@gmail.com>
2023-01-09 11:23:54 +0100

    add detailed cheese

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -2,3 +2,7 @@ from django.views.generic import ListView, DetailView
 from .models import Cheese
 class CheeseListView(ListView):
     model = Cheese
+
+class CheeseDetailView(DetailView):
+    model = Cheese
+    
